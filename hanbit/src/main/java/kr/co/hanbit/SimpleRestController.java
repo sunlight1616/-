97c5499b619389479910e0efd85f6b6ae9c2dff7
sunlight1616 <lhy13537713269@gmail.com>
2025-06-10 16:33:28 +0900

    Add files via upload
    lhy的全部文件

--- hanbit/src/main/java/kr/co/hanbit/SimpleRestController.java
+++ hanbit/src/main/java/kr/co/hanbit/SimpleRestController.java
@@ -1,27 +1,36 @@
 package kr.co.hanbit;
 
-import org.springframework.web.bind.annotation.RequestAttribute;
+import jdk.jfr.Registered;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class SimpleRestController {
-
     @RequestMapping("/article")
-    public String createArticle(@RequestParam("title") String title,
-                                @RequestParam("content") String content) {
-        return String.format("title=%s / content=%s", title, content);
+    public String createArticle(@RequestParam("title")String title,
+                                @RequestParam("content")String content){
+        return String.format("title=%s/content=%s",title,content);
     }
+
+
 
     @RequestMapping("/")
     public String Hello(){
-        return "<h1>Hello</h1> <h3><strong>Backend</strong></h3>";
+        return "<h1>Hello<h1> <h3><strong>Backend</strong><h3>";
     }
+
 
     @RequestMapping("/bye")
     public String Bye(){
         return "Bye";
     }
-}
 
+
+    @RequestMapping("/lihuiyong")
+    public String Myinfo(){
+        return "lihuiyong";
+    }
+}
